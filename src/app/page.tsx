27a027b73156578a3046b4d57bf01e47cb33b5ea
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-11-06 21:12:34 +0530

    my-second-machine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { db } from "@/utils/db";
 import Image from "next/image";
+import React from "react";
+import { NextPage } from "next";
 
-export default async function Home() {
+export default async function Home(){
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world</h1>
+      <h1>Hello world...</h1>
     </main>
-  );
+  ) ;
 }
