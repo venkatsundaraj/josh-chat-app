bf9d0e1047aa7e3a9217f2bbb50bc8c0c9332d16
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-11-16 20:47:39 +0530

    d-chat-deleted

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     domains: ['lh3.googleusercontent.com'],
   },
   reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
