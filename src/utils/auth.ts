849098a570f93a7e23cd3fa40ce610c21b49d242
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-11-27 18:04:47 +0530

    course-form-development

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -69,7 +69,7 @@ export const authOptions: NextAuthOptions = {
       return session
     },
     redirect() {
-      return `${process.env.NEXTAUTH_URL}/dashboard`
+      return `/dashboard`
     },
   },
 }
